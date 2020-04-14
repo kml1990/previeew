906bd9956eb4f8e097b2d934a31f5d5e8a5d34d7
Kamil <kjstepaniak1@gmail.com>
2020-04-14 23:07:31 +0100

    Updates

---
+++ precache-manifest.66ca201ce947da46047e218f990beeaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95cd0527d32eb4e0a38edcab0a20451e",
+    "url": "/previeew/index.html"
+  },
+  {
+    "revision": "3d0eaac2cc5b207fc8c1",
+    "url": "/previeew/static/css/main.61556d0a.chunk.css"
+  },
+  {
+    "revision": "bc1f983a5007a7996614",
+    "url": "/previeew/static/js/2.0ebcf0af.chunk.js"
+  },
+  {
+    "revision": "8bb64a847f9aad211d34d16da69f89b6",
+    "url": "/previeew/static/js/2.0ebcf0af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d0eaac2cc5b207fc8c1",
+    "url": "/previeew/static/js/main.8d7d82d8.chunk.js"
+  },
+  {
+    "revision": "b1896217a4e1dc87387b",
+    "url": "/previeew/static/js/runtime-main.dce02d1c.js"
+  }
+]);
